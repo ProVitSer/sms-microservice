1c72ac7beccf62ed7d051758c0e31d74976d551f
ProVitSer <prokinvs@gmail.com>
2023-09-23 19:55:47 +0000

    add new modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import configuration from './config/config.provider';
 import { SmsModule } from './sms/sms.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { getMongoUseFactory } from './config/mongo.config';
+import { SmsConfigModule } from './sms-config/sms-config.module';
+import { CacheModule } from './cache/cache.module';
 
 @Module({
   imports: [
@@ -16,8 +16,10 @@ import { getMongoUseFactory } from './config/mongo.config';
       inject: [ConfigService],
     }),
     SmsModule,
+    SmsConfigModule,
+    CacheModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
