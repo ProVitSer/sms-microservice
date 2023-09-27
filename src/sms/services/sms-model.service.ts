20edc7fa4b3ded20ccb0d048f40adb5ecd62d148
ProVitSer <prokinvs@gmail.com>
2023-09-27 21:32:31 +0000

    feat: find\update

--- src/sms/services/sms-model.service.ts
+++ src/sms/services/sms-model.service.ts
@@ -5,13 +5,22 @@ import { Sms, SmsDocument } from '../schemas/sms.schema';
 
 @Injectable()
 export class SmsModelService {
-    constructor(@InjectModel(Sms.name) private SmsModel: Model<SmsDocument>) {}
+    constructor(@InjectModel(Sms.name) private smsModel: Model<SmsDocument>) {}
 
     public async create(data: Sms): Promise<Sms> {
-        const smsTimeRangesConfig = new this.SmsModel({ ...data });
+        const smsTimeRangesConfig = new this.smsModel({ ...data });
 
         await smsTimeRangesConfig.save();
 
         return smsTimeRangesConfig.toObject();
+    }
+
+    public async find(filter: { [key: string]: any }, projection?: { [key in keyof Sms]: number }): Promise<Sms[]> {
+        return await this.smsModel.find(filter, projection).exec();
+    }
+
+    public async update(filter: { [key: string]: any }, data: { [key: string]: any }) {
+        data.changed = new Date();
+        return await this.smsModel.updateOne({ ...filter }, { $set: { ...data } });
     }
 }
