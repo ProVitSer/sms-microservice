e2d8a7e48a7f69376eedb60a862539d12c9aa2ec
ProVitSer <prokinvs@gmail.com>
2023-10-04 20:30:52 +0000

    feat: начальные интерфейсы по sms-aero

---
+++ src/sms-api/providers/sms-aero/interfaces/sms-aero.interfaces.ts
@@ -0,0 +1,44 @@
+import { SmsAeroSmsStatus } from './sms-aero.enum';
+
+export interface SmsAeroBaseResponse<T> {
+    success: boolean;
+    data: T | T[];
+    message: string | null;
+}
+
+export interface SmsAeroSendSmsData {
+    number?: string;
+    numbers?: string[];
+    sign: string;
+    text: string;
+    dateSend?: number;
+    callbackUrl?: string;
+    shortLink?: number;
+}
+
+export interface SmsAeroSendSmsResponse {
+    id: number;
+    from: string;
+    number: string;
+    text: string;
+    status: SmsAeroSmsStatus;
+    extendStatus: string;
+    channel: string;
+    cost?: number;
+    dateCreate: number;
+    dateSend: number;
+}
+
+export interface SmsAeroCheckSmsStatusParams {
+    id: number;
+}
+
+export interface SmsAeroCheckSmsStatusResponse extends SmsAeroSendSmsResponse {
+    dateAnswer: number;
+}
+
+export interface SmsAeroCallbackSendingResult {
+    id: string;
+    status: string;
+    extendStatus: string;
+}
