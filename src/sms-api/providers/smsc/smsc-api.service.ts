27b6f3482a78df3b97c0f8816665cea81f145a9d
ProVitSer <prokinvs@gmail.com>
2023-10-04 20:31:54 +0000

    ref types

--- src/sms-api/providers/smsc/smsc-api.service.ts
+++ src/sms-api/providers/smsc/smsc-api.service.ts
@@ -1,29 +1,30 @@
 import { HttpRequestService } from '@app/http-request/http.service';
 import { Injectable } from '@nestjs/common';
-import { SEND_SMS_URL, CHECK_SMS_STATUS_URL, SEND_MASS_JOB_SMS_URL } from './smsc.config';
+import { SMSC_SEND_SMS_URL, SMSC_CHECK_SMS_STATUS_URL, SMSC_SEND_MASS_JOB_SMS_URL } from './smsc.config';
 import {
-    CheckSmsStatusResponse,
-    SendSmsJob,
-    SendSmsResponse,
-    SmsCheckSmsStatusParams,
+    SmscCheckSmsStatusResponse,
+    SmscSendSmsJob,
+    SmscSendSmsResponse,
+    SmscCheckSmsStatusParams,
     SmscSendSmsData,
 } from './interfaces/smsc.interfaces';
 
 @Injectable()
 export class SmscApiService {
+    private readonly customRequestConfig = {
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    };
     constructor(private readonly http: HttpRequestService) {}
 
-    public async sendSmscSms(requestData: SmscSendSmsData): Promise<SendSmsResponse> {
+    public async sendSmscSms(requestData: SmscSendSmsData): Promise<SmscSendSmsResponse> {
         try {
-            return await this.http.post<SmscSendSmsData, SendSmsResponse>(
+            return await this.http.post<SmscSendSmsData, SmscSendSmsResponse>(
                 { data: requestData },
                 {
-                    url: SEND_SMS_URL,
-                    customRequestConfig: {
-                        headers: {
-                            'Content-Type': 'application/json',
-                        },
-                    },
+                    url: SMSC_SEND_SMS_URL,
+                    customRequestConfig: this.customRequestConfig,
                 },
             );
         } catch (e) {
@@ -31,17 +32,21 @@ export class SmscApiService {
         }
     }
 
-    public async checkStatus(requestParams: SmsCheckSmsStatusParams): Promise<CheckSmsStatusResponse> {
+    public async checkStatus(requestParams: SmscCheckSmsStatusParams): Promise<SmscCheckSmsStatusResponse> {
         try {
-            return await this.http.get<CheckSmsStatusResponse>(CHECK_SMS_STATUS_URL, { customRequestConfig: { params: requestParams } });
+            return await this.http.get<SmscCheckSmsStatusResponse>(SMSC_CHECK_SMS_STATUS_URL, {
+                customRequestConfig: { params: requestParams },
+            });
         } catch (e) {
             throw e;
         }
     }
 
-    public async sendJobs(requestParams: SendSmsJob): Promise<SendSmsResponse> {
+    public async sendJobs(requestParams: SmscSendSmsJob): Promise<SmscSendSmsResponse> {
         try {
-            return await this.http.get<CheckSmsStatusResponse>(SEND_MASS_JOB_SMS_URL, { customRequestConfig: { params: requestParams } });
+            return await this.http.get<SmscCheckSmsStatusResponse>(SMSC_SEND_MASS_JOB_SMS_URL, {
+                customRequestConfig: { params: requestParams },
+            });
         } catch (e) {
             throw e;
         }
--- src/sms-api/providers/smsc/smsc.config.ts
+++ src/sms-api/providers/smsc/smsc.config.ts
@@ -1,3 +1,3 @@
-export const SEND_SMS_URL = 'https://smsc.ru/rest/send/';
-export const CHECK_SMS_STATUS_URL = 'https://smsc.ru/sys/status.php';
-export const SEND_MASS_JOB_SMS_URL = 'https://smsc.ru/sys/jobs.php';
+export const SMSC_SEND_SMS_URL = 'https://smsc.ru/rest/send/';
+export const SMSC_CHECK_SMS_STATUS_URL = 'https://smsc.ru/sys/status.php';
+export const SMSC_SEND_MASS_JOB_SMS_URL = 'https://smsc.ru/sys/jobs.php';
