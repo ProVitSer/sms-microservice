e5abe9e2314192f93ad00ad8fc88b0baad4cd171
ProVitSer <prokinvs@gmail.com>
2023-10-05 20:47:23 +0000

    fix name

--- src/sms-config/controllers/sms-config.controller.ts
+++ src/sms-config/controllers/sms-config.controller.ts
@@ -2,7 +2,7 @@ import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Post } from '@nest
 import { SmsConfigService } from '../services/sms-config.service';
 import { UUIDParam } from '../../decorators/uuid-param.decorator';
 import { SmsConfignDto } from '../dto/sms-config.dto';
-import { SmsConfignActivateDto } from '../dto/sms-config-active.dto';
+import { SmsConfigActivateDto } from '../dto/sms-config-active.dto';
 
 @Controller('sms-config')
 export class SmsConfigController {
@@ -25,7 +25,7 @@ export class SmsConfigController {
 
     @HttpCode(HttpStatus.OK)
     @Post('active')
-    async activateConfig(@Body() body: SmsConfignActivateDto) {
+    async activateConfig(@Body() body: SmsConfigActivateDto) {
         return await this.smsConfigService.modifyActive(body);
     }
 }
--- src/sms-config/dto/sms-config-active.dto.ts
+++ src/sms-config/dto/sms-config-active.dto.ts
@@ -1,6 +1,6 @@
 import { IsBoolean, IsNotEmpty, IsString } from 'class-validator';
 
-export class SmsConfignActivateDto {
+export class SmsConfigActivateDto {
     @IsString()
     @IsNotEmpty()
     clientId: string;
