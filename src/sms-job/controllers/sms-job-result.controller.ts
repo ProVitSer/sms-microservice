dde1e722bf0a7ad7c970669673e3cc1062b62e48
ProVitSer <prokinvs@gmail.com>
2023-10-09 20:07:37 +0000

    feat: smsJobId для обработки резальтатов доставки

--- src/sms-job/controllers/sms-job-result.controller.ts
+++ src/sms-job/controllers/sms-job-result.controller.ts
@@ -2,21 +2,24 @@ import { Body, Controller, HttpStatus, Param, Post, Res } from '@nestjs/common';
 import { SmsJobService } from '../services/sms-job.service';
 import { UUIDParam } from '@app/decorators/uuid-param.decorator';
 import { SmsApiProviderType } from '@app/sms-config/interfaces/sms.-config.enum';
-import { IncomingSmsSendingResult } from '@app/sms-api/interfaces/sms-api.interfaces';
 import { Response } from 'express';
+import { IncomingSmsSendingResult } from '@app/sms-api/interfaces/sms-api.interfaces';
 
 @Controller('sms-job')
 export class SmsJobResultController {
     constructor(private readonly smsJobService: SmsJobService) {}
 
-    @Post('result/:smsApiProvider/:clientId')
+    @Post('result/:smsJobId/:smsApiProvider/:clientId')
     async setSmsResult(
         @UUIDParam('clientId') clientId: string,
+        @UUIDParam('smsJobId') smsJobId: string,
         @Param('smsApiProvider') smsApiProvider: SmsApiProviderType,
         @Body() body: IncomingSmsSendingResult,
         @Res() res: Response,
     ) {
         try {
+            console.log(JSON.stringify(body));
+            this.smsJobService.setJobResult(body, { clientId, smsJobId, smsApiProvider });
             return res.sendStatus(HttpStatus.OK);
         } catch {
             return res.sendStatus(HttpStatus.SERVICE_UNAVAILABLE);
